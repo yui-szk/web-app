10019a51cc5c783bc713502aa7ca16902b33998f
yui-szk <yuisuzuki017@gmail.com>
2024-02-19 19:00:55 +0900

    formタグを更新

--- myapp/resources/views/create.php
+++ myapp/resources/views/create.php
@@ -10,9 +10,9 @@
     <body>
         <h1>Create Page</h1>
         <div style="display: flex">
-            <form style="margin: 12px;">
-                <input type="text" placeholder="add a task" style="margin: 20px; padding: 4px;">
-                <input type="date">
+            <form action="" method="POST" style="margin: 12px;">
+                <input type="text" name="name" placeholder="add a task" style="margin: 20px; padding: 4px;">
+                <input type="date" name="deadline_date">
                 <button type="submit" style="margin: 8px; padding:4px">Add</button>
                 <textarea name="details" id="details" placeholder="details" cols="30" rows="10" style="margin: 24px; height:60px"></textarea>
             </form>
