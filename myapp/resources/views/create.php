bde14048a6267711e9769d390c450c1b86bb883f
yui-szk <yuisuzuki017@gmail.com>
2024-02-19 18:15:00 +0900

    form追加

--- myapp/resources/views/create.php
+++ myapp/resources/views/create.php
@@ -1,22 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Create Page</title>
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Create Page</title>
+    </head>
 
-<body>
-    <h1>Create Page</h1>
-    <div style="display: flex">
-        <div style="margin: 12px;">
-            <input type="text" placeholder="add a task" style="margin: 20px; padding: 4px;">
-            <input type="date">
-            <button type="submit" style="margin: 8px; padding:4px">Add</button>
-            <textarea name="details" id="details" placeholder="details" cols="30" rows="10" style="margin: 24px; height:60px"></textarea>
+    <body>
+        <h1>Create Page</h1>
+        <div style="display: flex">
+            <form style="margin: 12px;">
+                <input type="text" placeholder="add a task" style="margin: 20px; padding: 4px;">
+                <input type="date">
+                <button type="submit" style="margin: 8px; padding:4px">Add</button>
+                <textarea name="details" id="details" placeholder="details" cols="30" rows="10" style="margin: 24px; height:60px"></textarea>
+            </form>
         </div>
-    </div>
-</body>
-
+    </body>
 </html>
+
