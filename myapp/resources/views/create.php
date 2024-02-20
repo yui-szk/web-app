1ccc652b1e0873693c3d6297b69ac18ee38d3570
yui-szk <yuisuzuki017@gmail.com>
2024-02-20 16:43:49 +0900

    brタグ追加

--- myapp/resources/views/create.php
+++ myapp/resources/views/create.php
@@ -14,7 +14,8 @@
                 <input type="text" name="name" placeholder="add a task" style="margin: 20px; padding: 4px;">
                 <input type="date" name="deadline_date">
                 <button type="submit" style="margin: 8px; padding:4px">Add</button>
-                <textarea name="details" id="details" placeholder="details" cols="30" rows="10" style="margin: 24px; height:60px"></textarea>
+                <br/>
+                <textarea name="details" id="details" placeholder="details" cols="30" rows="10" style="margin: 20px; height:60px"></textarea>
             </form>
         </div>
     </body>
