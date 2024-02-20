aea201bd750c5bf1b9a6bf457e6fdf43c63d91a1
yui-szk <yuisuzuki017@gmail.com>
2024-02-20 16:57:29 +0900

    データベース名を変更

--- myapp/app/Models/Task.php
+++ myapp/app/Models/Task.php
@@ -6,10 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $connection = "tasks";
-
+    protected $connection = "my_database";
     protected $table = "tasks";
-    protected $primaryKey = "id";
 }
-
-
