4b34b34f5b1761738f9613742e42f2a89064faac
yui-szk <yuisuzuki017@gmail.com>
2024-03-13 00:22:34 +0900

    $id->$request

--- myapp/app/Http/Controllers/TaskController.php
+++ myapp/app/Http/Controllers/TaskController.php
@@ -2,17 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Models\Task;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-
 
 class TaskController extends Controller
 {
     public function create(Request $request): RedirectResponse
     {
-        $task = new Task;
+        $task = new Task();
 
         $task->name = $request->name;
         $task->deadline_date = $request->deadline_date;
@@ -23,18 +21,23 @@ class TaskController extends Controller
         return redirect('/list');
     }
 
-    public function show(){
+    public function show()
+    {
         $tasks = Task::all();
+
         return view('list', compact('tasks'));
     }
 
-    public function edit($id){
+    public function edit($id)
+    {
         $task = Task::find($id);
+
         return view('edit', compact('task'));
     }
 
-    public function update($id, Request $request){
-        $task = Task::find($id);
+    public function update(Request $request)
+    {
+        $task = Task::find($request->id);
         $task->name = $request->name;
         $task->deadline_date = $request->deadline_date;
         $task->save();
@@ -42,8 +45,11 @@ class TaskController extends Controller
         return redirect('/list');
     }
 
-    public function delete($id){
-        Task::find($id)->delete();
+    public function delete(Request $request)
+    {
+        $task = Task::find($request->id);
+        $task->delete();
+
         return redirect('/list');
     }
 }
