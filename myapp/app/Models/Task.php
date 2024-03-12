d50485ad6ec553f521ca27054536665294325c07
yui-szk <yuisuzuki017@gmail.com>
2024-03-13 00:23:11 +0900

    フォーマット

--- myapp/app/Models/Task.php
+++ myapp/app/Models/Task.php
@@ -7,12 +7,16 @@ use Illuminate\Support\Facades\DB;
 
 class Task extends Model
 {
-    protected $connection = "my_database";
-    protected $table = "tasks";
+    protected $connection = 'my_database';
+
+    protected $table = 'tasks';
+
     protected $guarded = ['id', 'created_at', 'updated_at'];
 
-    public function getData(){
+    public function getData()
+    {
         $data = DB::table($this->table)->get();
+
         return $data;
     }
 }
--- myapp/resources/views/create.blade.php
+++ myapp/resources/views/create.blade.php
@@ -1,21 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Create Page</title>
-    </head>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Create Page</title>
+</head>
 
-    <body>
-        <h1>Create Page</h1>
-        <div style="display: flex">
-            <form method="POST" action="/create" style="margin: 12px;">
-                @csrf
-                <input type="text" name="name" placeholder="add a task" style="margin: 20px; padding: 4px;">
-                <input type="date" name="deadline_date">
-                <button type="submit" style="margin: 8px; padding:4px">Add</button>
-            </form>
-        </div>
-    </body>
+<body>
+    <h1>Create Page</h1>
+    <div style="display: flex">
+        <form method="POST" action="/create" style="margin: 12px;">
+            @csrf
+            <input type="text" name="name" placeholder="add a task" style="margin: 20px; padding: 4px;">
+            <input type="date" name="deadline_date">
+            <button type="submit" style="margin: 8px; padding:4px">Add</button>
+        </form>
+    </div>
+</body>
+
 </html>
