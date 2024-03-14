967481960f3ff32402e36935d39e6af95b5d08b8
yui-szk <yuisuzuki017@gmail.com>
2024-03-15 00:59:19 +0900

    DB初期化処理

--- mariadb/init/create_tasks_table.sql
+++ mariadb/init/create_tasks_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS tasks (
   id int(10) PRIMARY KEY auto_increment,
   name VARCHAR(255) NOT NULL,
-  deadline_date VARCHAR(255) NOT NULL,
+  deadline VARCHAR(255),
   created_at VARCHAR(255) NOT NULL,
   updated_at VARCHAR(255) NOT NULL,
   status tinyint(1) NOt NULL
