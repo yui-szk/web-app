8e97377aae85bddf6d60da3fa3f562dfa270c8c0
yui-szk <yuisuzuki017@gmail.com>
2024-03-17 18:00:25 +0900

    不使用のRouteを削除

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -3,19 +3,5 @@
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/create', function () {
-//     return view('create');
-// });
-
-// Route::post('/create', [TaskController::class, 'create']);
-
-// Route::get('/list', [TaskController::class, 'show']);
-// Route::delete('/list', [TaskController::class, 'delete']);
-// Route::put('/list', [TaskController::class, 'update']);
-
-// Route::get('/edit/{id}', [TaskController::class, 'edit']);
-
-// Route::post('/list', [TaskController::class, 'show']);
-
 Route::resource('tasks', TaskController::class);
 Route::redirect('/', '/tasks');
