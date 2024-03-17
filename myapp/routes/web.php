a891bcacc17860e499f97c32bad91785fe4e43ab
yui-szk <yuisuzuki017@gmail.com>
2024-03-17 17:58:42 +0900

    /から/tasksにredirectを追加

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -18,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 // Route::post('/list', [TaskController::class, 'show']);
 
 Route::resource('tasks', TaskController::class);
+Route::redirect('/', '/tasks');
